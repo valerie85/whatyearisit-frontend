55cd9865df1b182c5f68a604bf509a7ff64df202
Your name <Your email>
2024-09-20 16:26:33 +0200

    last

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ fetch('https://whatyearisit-backend-smoky.vercel.app/year')
 	.then(response => response.json())
 	.then(data => {
         document.querySelector('#year').innerHTML = `
-				<div id="year"> year: ${data.year} </div>`;
+				<div id="year"> year:"${data.year}" </div>`;
     });
 
